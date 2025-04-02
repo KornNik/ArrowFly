59bee9c888bec8d50e7003dde0082f95e24c5ce0
KornNik <kornienkokolya23@gmail.com>
2025-04-02 17:06:16 +0300

    Early version

---
+++ ArrowFly/Assets/Scripts/Behaviours/ArrowTorque.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Behaviours
+{
+    sealed class ArrowTorque : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        private Vector2 _velocity;
+        private float _angle;
+
+        private void FixedUpdate()
+        {
+            _velocity = _rigidbody.velocity;
+            _angle = Mathf.Atan2(_velocity.y, _velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
+        }
+
+        public void Throw(Vector2 velocity)
+        {
+            _rigidbody.simulated = true;
+            _rigidbody.velocity = _velocity;
+        }
+    }
+}
---
+++ ArrowFly/Assets/Scripts/Behaviours/StateMachine/LoadLevelState.cs
@@ -0,0 +1,50 @@
+﻿using Helpers;
+using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Behaviours
+{
+    sealed class LoadLevelState : BaseState
+    {
+        private ILevelLoader _levelLoader;
+        private Launcher _launcher;
+        private ArrowTorque _arrowTorque;
+
+        public LoadLevelState(GameStateController stateController) : base(stateController)
+        {
+            _levelLoader = Services.Instance.LevelLoader.ServicesObject;
+        }
+
+        public override void EnterState()
+        {
+            base.EnterState();
+            LoadAll().Forget();
+        }
+        private async UniTaskVoid LoadAll()
+        {
+            await LoadTask(LoadLevelBehaviours);
+            await LoadTask(LoadBowAndArrow);
+            await LoadTask(StartGameState);
+        }
+
+        private async UniTask LoadTask(Action loadingAction)
+        {
+            loadingAction?.Invoke();
+            await UniTask.Yield();
+        }
+        private void LoadLevelBehaviours()
+        {
+            _levelLoader.LoadLevelByIndex(0);
+        }
+        private void LoadBowAndArrow()
+        {
+            _arrowTorque =  GameObject.Instantiate(Services.Instance.DataResourcePrefabs.ServicesObject.GetArrow());
+            _launcher = GameObject.Instantiate(Services.Instance.DataResourcePrefabs.ServicesObject.GetLauncher());
+        }
+        private void StartGameState()
+        {
+            ChangeGameStateEvent.Trigger(GameStateType.GameState);
+        }
+    }
+}
